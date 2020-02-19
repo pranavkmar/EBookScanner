d1f96bfca22db989236bcc981f477d5cfd7162bd
Pranav <informpranav@gmail.com>
2020-02-19 17:29:41 +0530

    Flask WebServer files

--- Python/raspPi.py
+++ Python/raspPi.py
@@ -8,26 +8,30 @@
 # import time
 
 # Setup Part
-# reed = 18
+import GPIO as GPIO
+
+reed = 18
 # send_capture_signal = 22
 # GPIO.setmode(GPIO.BOARD)
 # GPIO.setup(reed, GPIO.IN)
 # GPIO.setup(send_capture_signal, GPIO.OUT)
 # Infinite Loop
 try:
-	while True:
-		# Check if Reed Sensor Pin is Lowx
-		reed _state = GPIO.input(reed)
-		if reed _state == False
-			print('Now door is open..')
-			GPIO.output(buz, True) # Switch on buzzer
-		else: 
-			# Reed switch is closed so stop buzzer
-			GPIO.output(buz,False)
-			time.sleep(0.5) # Wait for half a second before next check
+    while True:
+        # Check if Reed Sensor Pin is Lowx
+        reed
+        _state = GPIO.input(reed)
+        if reed _state == False
+        print('Now door is open..')
+        GPIO.output(buz, True)  # Switch on buzzer
+    else:
+        # Reed switch is closed so stop buzzer
+        GPIO.output(buz, False)
+        time.sleep(0.5)  # Wait for half a second before next check
 
 except KeyboardInterrupt:
-	print ‘CTRL+C Pressed’
-	GPIO.output(buz, False) # Switch of the buzzer
-	GPIO.cleanup()
-	# Clean up and release all GPIO Pins
+    print ("CTRL + C Pressed")
+    GPIO.output(buz, False)
+# Switch of the buzzer
+# GPIO.cleanup()
+# Clean up and release all GPIO Pins
